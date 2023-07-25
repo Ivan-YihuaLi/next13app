219f48b84cb676457620ae2dfc2399153831f2b5
Yihua Li <yihuali93@gmail.com>
2023-07-25 15:12:03 -0700

    resolved conflict

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from './navbar'
+import Navbar from '../../components/Navbar'
 
 export const metadata = {
   title: 'Dashboard',
--- app/dashboard/navbar.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-const Navbar = () => {
-  return <nav className="p-4">Navbar</nav>
-}
-
-export default Navbar
