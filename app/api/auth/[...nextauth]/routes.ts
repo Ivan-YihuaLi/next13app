2503997d329c1f438e47e949cb33c2ff1f8ecbdb
Yihua Li <yihuali93@gmail.com>
2023-07-28 15:45:20 -0700

    fixed type errors

--- app/api/auth/[...nextauth]/routes.ts
+++ app/api/auth/[...nextauth]/routes.ts
@@ -5,8 +5,8 @@ const handler = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
 });
